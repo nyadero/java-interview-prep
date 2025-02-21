d25bc7ad9ae92884fb11f268cf8487bb02f700ff
nyadero <briannyadero443@@gmail.com>
2025-02-21 14:24:22 +0300

    describe the problem

--- src/coding_challenges/MissingNumber.java
+++ src/coding_challenges/MissingNumber.java
@@ -2,6 +2,7 @@ package coding_challenges;
 
 import java.util.Arrays;
 
+//Problem: Given an array nums containing n distinct numbers in the range [0, n], return the missing number.
 public class MissingNumber {
     public static void main(String[] args) {
         System.out.println(findMissingNumber(new int[]{0, 2,1, 3, 5}));
