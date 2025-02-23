d12e6cc8a1eda49be49138adf20f4f19fdd4eba6
nyadero <briannyadero443@@gmail.com>
2025-02-23 16:23:20 +0300

    optimize imports

--- src/coding_challenges/ReverseString.java
+++ src/coding_challenges/ReverseString.java
@@ -1,7 +1,5 @@
 package coding_challenges;
 
-import java.util.Arrays;
-
 public class ReverseString {
     public static void main(String[] args) {
         System.out.println(reverseString("hello"));
