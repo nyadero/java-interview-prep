510aba3769566d83500417a7655bf31786dcbaca
nyadero <briannyadero443@@gmail.com>
2025-02-23 16:22:51 +0300

    most frequented char in a string method

--- src/coding_challenges/MostFrequentElement.java
+++ src/coding_challenges/MostFrequentElement.java
@@ -8,11 +8,12 @@ import java.util.Map;
 public class MostFrequentElement {
     public static void main(String[] args) {
         System.out.println(mostFrequentElement(new int[]{1,2,5,3,4,5,}));
+        System.out.println(mostFrequentChar("Hello"));
     }
 
     public static int mostFrequentElement(int[] nums){
         int mostCount = 0;
-        int mostFrequent = -1;
+        Integer mostFrequent = null;
         Map<Integer, Integer> map = new HashMap<Integer,Integer>();
         for (int num : nums) {
             // Get the current count, defaulting to 0 if the key doesn't exist
@@ -27,4 +28,21 @@ public class MostFrequentElement {
 
         return mostFrequent;
     }
+
+    public static char mostFrequentChar(String s) {
+        int mostCount = 0;
+        char mostFrequent = '\0';
+        Map<Character, Integer> map = new HashMap<>();
+
+        for (char c : s.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
+
+            if (map.get(c) > mostCount) {
+                mostFrequent = c;
+                mostCount = map.get(c);
+            }
+        }
+        return mostFrequent;
+    }
+
 }
